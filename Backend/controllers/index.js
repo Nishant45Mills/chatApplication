be7de8aa7456dc5ecb56e4a9f94059addfd6e3cc
Nishant jarang <nishantjarang74@gmail.com>
2024-07-19 15:29:52 +0530

    first commit

--- Backend/controllers/index.js
+++ Backend/controllers/index.js
@@ -1,4 +1,5 @@
 module.exports.auth = require("./auth.controller");
 module.exports.user = require("./user.controller");
 module.exports.chat = require("./chat.controller");
+//hello
 module.exports.message = require("./message.controller");
